4d803753c060553b3d3adfd73737a5238525828c
Jayasmita123 <61414785+Jayasmita123@users.noreply.github.com>
2020-09-08 19:29:40 +0530

    Add files via upload

---
+++ navigation-bar.php
@@ -0,0 +1,77 @@
+<html>
+    <head>
+<!--        meta data-->
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1 ">
+        
+        
+<!--        bootstrap file-->
+            <link rel="stylesheet" href="css/bootstrap.min.css" >
+        
+        
+<!--        javascript file-->
+                <script src="bootstrap/jquery.js"></script>
+                <script src="js/bootstrap.min.js"></script>
+        
+        
+<!--        external css-->
+            <link rel="stylesheet" type="text/css" href="custom.css">
+
+<!--        animation file-->
+        <link href="bootstrap/animation.css" type="text/css" rel="stylesheet">
+        <script src="bootstrap/animation.js"></script>
+<!--        owl carousel file-->
+        <link rel="stylesheet" href="dist/assets/owl.carousel.min.css">
+        <link rel="stylesheet" href="dist/assets/owl.theme.green.min.css">
+        <link rel="stylesheet" href="dist/assets/owl.theme.default.min.css">
+          <script src="dist/owl.carousel.min.js"></script>
+        
+        
+<!--        font awesome-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        	
+            </head>
+    <body>
+
+                  <script>
+  AOS.init();
+</script>
+<!--        navigation bar-->
+          
+            <nav class="navbar navbar-expand-sm navbar-light ">
+                <a class="navbar-brand" href="#" id="asdf">
+                    <img class="img-fluid" src="images/lg.jpg" style="width:100px;" id="navbarlogo"> 
+                </a>
+          
+                <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"   >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class=" nav navbar-nav ml-auto" id="list">
+                        <li class="nav-item  ">
+                            <a class="nav-link " href="index.php" ><strong >Home</strong></a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link  " href="product.php"><strong >Product</strong></a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link  " href="pricing.php"><strong >Pricing</strong></a>
+                        </li>  
+                        <li class="nav-item ">
+                            <a class="nav-link  " href="about-us.php"><strong >Our story</strong></a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link " href="news.php"><strong >News</strong></a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link  " href="contact-us.php"><strong>Contact</strong></a>
+                        </li>   
+                        <li class="nav-item ">
+                            <a class="nav-link " href="server.php"><strong ><i class="fa fa-search" style="font-size:20px;" aria-hidden="true"></i></strong></a>
+                        </li>
+                    </ul>
+              </div> 
+            </nav>
+       
+    </body>
+</html>
